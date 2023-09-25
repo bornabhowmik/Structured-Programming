c14a62b250fda36bdf54dbb4d5cd870c6bab858e
bornabhowmik <bornabhowmik4@gmail.com>
2023-09-26 01:44:20 +0600

    nested.c code added

---
+++ Arithmatic_Operators/nested.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if(tk>=5000)
+    {
+        printf("Cox's Bazer jabo");
+        if(tk>-1000)
+        {
+            printf("saint Mertine jabo\n");
+        }
+        else
+        {
+            printf("Ferot chole jabo\n");
+        }
+    }
+    else
+    {
+        printf("Kothao jabo na\n");
+    }
+
+    return 0;
+}
