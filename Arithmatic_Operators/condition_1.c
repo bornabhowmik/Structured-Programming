f99e0f73fccee603a9167d465ca5c6965aaf9018
bornabhowmik <bornabhowmik4@gmail.com>
2023-09-26 01:29:49 +0600

    condition_1.c code added

---
+++ Arithmatic_Operators/condition_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+
+    if(tk==100)
+    {
+        printf("Berger khabo");
+    }
+
+    else
+    {
+        printf("kicho khabo na");
+    }
+
+    return 0;
+}
