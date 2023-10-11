7ccd9c0b9c3c6fc51886acec2f1617f68ff0b4f2
bornabhowmik <bornabhowmik4@gmail.com>
2023-10-11 11:37:56 +0600

    B.Searching.c code added

---
+++ Problem Solving in codeforces_C/output/B.Searching.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int x;
+    scanf("%d", &x);
+    
+    int ans=-1;
+
+    for(int i=0; i<n; i++){
+        if(x==arr[i]){
+            ans=i;
+            break;
+        }
+    }
+    printf("%d\n", ans);
+
+    return 0;
+}
